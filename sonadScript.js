4b22cd9a9a7a9a1e72bb854ee5da4a964a30e124
MaksimTse <maksimplamja@gmail.com>
2024-03-25 14:07:30 +0200

    da

---
+++ sonadScript.js
@@ -0,0 +1,83 @@
+const sonad=[
+    'teostus',
+    'algoritm',
+    'kavand',
+    'kasutajaliides',
+    'mudel',
+    'andmebaasi skeem',
+    'standart',
+    'tsükkel',
+    'andmetöötlus',
+    'andmestruktuur',
+    'arenduskeskkond',
+    'projektihaldus tööriistad',
+    'kavandamine',
+    'iteratsioon',
+    'parandus',
+    'koskmudel',
+    'agiilne mudel',
+    'inkrementaalne mudel',
+    'nõudmised',
+    'realiseerimine',
+    'testimine',
+    'integreerimine',
+    'kasutamine',
+    'hooldus',
+    'eelised',
+    'puudused',
+    'elutsükkel',
+    'arendamine',
+    'valideerimine'
+];
+const venesonad=[
+    "реализация",
+    "алгоритм",
+    "план",
+    "пользовательский интерфейс",
+    "модель",
+    "схема базы данных",
+    "стандарт",
+    "цикл",
+    "оброботка данных",
+    "структруа базы данных",
+    "среда разработки",
+    "инструменты управления проектами",
+    "планирование",
+    "итерация",
+    "исправление",
+    "каскадная модель",
+    "агильная модель",
+    "инкрементальная модель",
+    "требования",
+    "реализация",
+    "тестирование",
+    "интегрирование",
+    "использование",
+    "обслуживание",
+    "преимущества",
+    "недостатки",
+    "жизненный цикл",
+    "разработка",
+    "валидирование"
+];
+function randomSona(){
+
+    const juhuslikSona=Math.floor(Math.random()*sonad.length);
+
+    //võtame juhuslik sõna massivist
+    const sona=sonad[juhuslikSona];
+    //lisame html lehele
+    document.getElementById("random-sona").innerHTML = sona;
+    window.correct= sona;
+}
+function checkSona(){
+    const check = document.getElementById("kontroll").value.trim().toLowerCase();
+    const correct = window.correct.toLowerCase();
+    const correctVene = venesonad[sonad.indexOf(correct)].toLowerCase();
+
+    if (check === correct || check === correctVene) {
+        document.getElementById("vastus").innerText = "Õige!";
+    } else {
+        document.getElementById("vastus").innerText = "Vale!";
+    }
+}
